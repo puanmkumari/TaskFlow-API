3dc9382f4f1f6d268e55834fecac0efdbedcd870
puanmkumari <punamkumari.hzb05@gmail.com>
2026-07-06 23:38:50 +0530

    Add files via upload

---
+++ src/utils/response.js
@@ -0,0 +1,17 @@
+const sendResponse = (
+    res,
+    statusCode,
+    success,
+    message,
+    data = null
+) => {
+
+    return res.status(statusCode).json({
+        success,
+        message,
+        data
+    });
+
+};
+
+module.exports = sendResponse;
